bc7bb73f12b16def8df4334f564fb8155b026246
Geoffry George Mathew <79758301+geoffrygeorge@users.noreply.github.com>
2021-06-01 17:42:32 +0300

    updated st.cache parameters in modelling.py

--- modelling.py
+++ modelling.py
@@ -66,7 +66,7 @@ class Modelling:
     # function defined for cleaning the dataset - time_series_covid19_confirmed_global.csv
     # SECOND DATA FRAME(INFECTED POPULATION DATA)
     @staticmethod
-    #@st.cache(show_spinner = False, ttl = 60)
+    #@st.cache(show_spinner = False)
     def infected_clean():
         url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
         infected_content = requests.get(url).content
@@ -85,7 +85,7 @@ class Modelling:
     # function defined for cleaning the dataset - time_series_covid19_recovered_global.csv
     # THIRD DATA FRAME(RECOVERED POPULATION DATA)
     @staticmethod
-    #@st.cache(show_spinner = False, ttl = 60)
+    #@st.cache(show_spinner = False)
     def recovered_clean():
         url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
         recovered_content = requests.get(url).content
@@ -104,7 +104,7 @@ class Modelling:
     # function defined for cleaning the dataset - time_series_covid19_deaths_global.csv
     # FOURTH DATA FRAME(DECEASED POPULATION DATA)
     @staticmethod
-    #@st.cache(show_spinner = False, ttl = 60)
+    #@st.cache(show_spinner = False)
     def deceased_clean():
         url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
         deceased_content = requests.get(url).content
